b0e5b60d3e53ba1e8349d3b92e6c4b72524df947
GeraldGR146 <gerald.rizkygr@gmail.com>
2024-12-02 08:31:55 +0700

    feat: add watchlist model

---
+++ models/watchlist.py
@@ -0,0 +1,13 @@
+from models.base import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Watchlist(Base):
+    __tablename__ = 'watchlist'
+    
+    id = Column(String, primary_key=True)
+    user_id = Column(String, ForeignKey('users.id'), nullable=False)
+    course_id = Column(String, ForeignKey('courses.id'), nullable=False)
+    
+    user = relationship("User", back_populates="watchlists")
+    course = relationship("Course", back_populates="watchlists")
